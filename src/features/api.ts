d5b87c8f00f83b294e8b30939f3cbc467478c4c3
Andriy Parashchuk <anrey.andreevich@gmail.com>
2026-04-16 11:21:39 +0200

    Add auto deploy to GH pages

--- src/features/api.ts
+++ src/features/api.ts
@@ -1,6 +1,11 @@
 import type { Transaction } from "./types";
 
 export const fetchTransactions = async (): Promise<Transaction[]> => {
-  const res = await fetch("/data.json");
+  const res = await fetch(`${import.meta.env.BASE_URL}data.json`);
+
+  if (!res.ok) {
+    throw new Error(`Loading failed: ${res.status}`);
+  }
+
   return res.json();
 };
--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 import { App } from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/wallet-app">
     <App />
   </BrowserRouter>,
 )
